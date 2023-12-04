a1918a8c3d974657a04a1a5af44f3cfca9ccd989
samet7ozturk <sametozturk973@gmail.com>
2023-12-05 01:12:36 +0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const engData = {
+  name: "almila",
+  header: "I am a Frontend Developer...",
+  heroRight: "/images/hero-right.png",
+  description:
+    "... who likes to craft solid and scalable frontend products with great user experiences",
+  links: ["Github", " Linkedin"],
+  basicInformation: {
+    birthDate: "24.03.1996",
+    city: "Ankara",
+    education: "Hacettepe Ünv, Biyoloji Lisans,2016",
+    preferredRole: "Frontend, UI",
+  },
+  informationTitles: [
+    { key: "birthDate", title: "Date of Birth" },
+    { key: "city", title: "City of Residence" },
+    { key: "education", title: "Education" },
+    { key: "preferredRole", title: "Preferred Role" },
+  ],
+  aboutMe: [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent accumsan, nisi in vehicula varius, eros leo fermentum orci, ac tincidunt ipsum magna eu nulla. Mauris ut molestie nisl, eget pulvinar tellus. Nulla tincidunt ornare massa, non pellentesque libero venenatis pretium. Morbi eu lectus semper, fringilla metus a, sagittis mauris. ",
+    "Phasellus tristique lacus neque, laoreet fermentum metus maximus luctus. Donec rutrum tellus vel augue tincidunt fringilla. Donec faucibus lacus in justo vulputate imperdiet. Proin tristique malesuada lectus sed semper. Suspendisse potenti. ",
+  ],
+  profileImg: "/images/profile.png",
+  skills: {
+    JAVASCRIPT: "/images/js.png",
+    REACT: "/images/react.png",
+    REDUX: "/images/redux.png",
+    NODE: "/images/nodejs.png",
+    VSCODE: "/images/VScode.png",
+    FIGMA: "/images/figma.png",
+  },
+
+  projects: ["Workintech", "Journey"],
+  projectsText: [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In neque ante, lobortis vitae tincidunt in, volutpat quis lacus. Sed sollicitudin nisi elit, vel auctor est cursus non. Quisque eu efficitur felis. Cras nisl ligula, tincidunt nec nulla nec, ultrices fringilla nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac suscipit arcu. Nullam nec tellus vitae erat iaculis varius ut eu libero. Nam lectus massa, malesuada in dignissim eu, semper a ipsum. ",
+    "Cras quis tortor in velit porta ultricies sed ut arcu. Proin volutpat nisl sapien, eget elementum nibh imperdiet et. Maecenas mi mi, maximus quis velit et, eleifend hendrerit nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras vel elementum eros. Donec risus mauris, scelerisque et purus maximus, ultrices sodales nibh. Vestibulum aliquet, tortor eu sagittis varius, metus nulla finibus nulla, quis pulvinar diam dui vel nisl. ",
+  ],
+  projectsImg: ["/images/Rectangle1.png", "/images/Rectangle2.png"],
+
+  contact: {
+    title: "Send me a message!",
+    text: "Got a question or proposal, or just want to say hello? Go ahead.",
+    email: "almilasucode@gmail.com",
+  },
+};
+
+export const trData = {
+  name: "almila",
+  header: "Ben bir Frontend Geliştiricisiyim...",
+  heroRight: "/images/hero-right.png",
+  description:
+    "... mükemmel kullanıcı deneyimlerine sahip, sağlam ve ölçeklenebilir frontend ürünleri oluşturmayı severim",
+  links: ["Github", " Linkedin"],
+  basicInformation: {
+    birthDate: "24.03.1996",
+    city: "Ankara",
+    education: "Hacettepe Ünv, Biyoloji Lisans,2016",
+    preferredRole: "Frontend, UI",
+  },
+  informationTitles: [
+    { key: "birthDate", title: "Doğum Tarihi" },
+    { key: "city", title: "İkamet Şehri" },
+    { key: "education", title: "Eğitim Durumu" },
+    { key: "preferredRole", title: "Tercih Ettiği Rol" },
+  ],
+  aboutMe: [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent accumsan, nisi in vehicula varius, eros leo fermentum orci, ac tincidunt ipsum magna eu nulla. Mauris ut molestie nisl, eget pulvinar tellus. Nulla tincidunt ornare massa, non pellentesque libero venenatis pretium. Morbi eu lectus semper, fringilla metus a, sagittis mauris. ",
+    "Phasellus tristique lacus neque, laoreet fermentum metus maximus luctus. Donec rutrum tellus vel augue tincidunt fringilla. Donec faucibus lacus in justo vulputate imperdiet. Proin tristique malesuada lectus sed semper. Suspendisse potenti. ",
+  ],
+  profileImg: "/images/profile.png",
+  skills: {
+    JAVASCRIPT: "/images/js.png",
+    REACT: "/images/react.png",
+    REDUX: "/images/redux.png",
+    NODE: "/images/nodejs.png",
+    VSCODE: "/images/VScode.png",
+    FIGMA: "/images/figma.png",
+  },
+
+  projects: ["Workintech", "Journey"],
+  projectsText: [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In neque ante, lobortis vitae tincidunt in, volutpat quis lacus. Sed sollicitudin nisi elit, vel auctor est cursus non. Quisque eu efficitur felis. Cras nisl ligula, tincidunt nec nulla nec, ultrices fringilla nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac suscipit arcu. Nullam nec tellus vitae erat iaculis varius ut eu libero. Nam lectus massa, malesuada in dignissim eu, semper a ipsum. ",
+    "Cras quis tortor in velit porta ultricies sed ut arcu. Proin volutpat nisl sapien, eget elementum nibh imperdiet et. Maecenas mi mi, maximus quis velit et, eleifend hendrerit nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras vel elementum eros. Donec risus mauris, scelerisque et purus maximus, ultrices sodales nibh. Vestibulum aliquet, tortor eu sagittis varius, metus nulla finibus nulla, quis pulvinar diam dui vel nisl. ",
+  ],
+  projectsImg: ["/images/Rectangle1.png", "/images/Rectangle2.png"],
+  contact: {
+    title: "Bana bir mesaj gönder!",
+    text: "Bir sorunuz veya teklifiniz mi var, yoksa sadece merhaba mı demek istiyorsunuz? Devam edin.",
+    email: "almilasucode@gmail.com",
+  },
+};
