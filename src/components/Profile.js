335524dbcf6041ae6b6356744f8f5bb9449dca1c
samet7ozturk <sametozturk973@gmail.com>
2023-12-06 15:56:50 +0300

    Profile component tailwind complete

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -19,29 +19,40 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="bg-white rounded-md shadow flex-1">
-      <div>
-        <h2>Profile</h2>
+    <div className="bg-[#4731D3]">
+      <div className="ml-[10%] pt-20">
+        <h2 className="text-[48px] text-[#CBF281] font-bold">Profile</h2>
       </div>
-      <div>
-        <h3>Basic Information</h3>
-      </div>
-      {data.informationTitles &&
-        data.informationTitles.map((item) => (
-          <div key={item.key}>
-            <p>
-              {item.title}: {data.basicInformation[item.key]}
-            </p>
+      <div className="flex">
+        <div className="w-1/3 ml-[10%]">
+          <div>
+            <h3 className="text-[30px] text-white font-medium mt-8 mb-4">
+              Basic Information
+            </h3>
           </div>
-        ))}
-      <div>
-        <img src={svg1} alt="svg1" />
-      </div>
-      <div>
-        <h3>About Me</h3>
-      </div>
-      <div>
-        <p>{data.aboutMe}</p>
+          {data.informationTitles &&
+            data.informationTitles.map((item) => (
+              <div key={item.key} className="flex">
+                <p className="text-[#CBF281] w-36 mb-4">{item.title}</p>
+                <p className="text-white w-36 mb-4">
+                  {data.basicInformation[item.key]}
+                </p>
+              </div>
+            ))}
+        </div>
+        <div className="w-1/3">
+          <img src={svg1} alt="svg1" />
+        </div>
+        <div className="w-1/3 ml-8 mr-[10%]">
+          <div>
+            <h3 className="text-[30px] text-white font-medium mt-8 mb-4">
+              About Me
+            </h3>
+          </div>
+          <div>
+            <p className="text-white">{data.aboutMe}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
